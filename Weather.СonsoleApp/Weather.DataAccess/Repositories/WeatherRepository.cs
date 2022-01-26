bb3e15ffde6cd23065756a07d485c715eaf3b7bf
alesyalis <66536759+alesyalis@users.noreply.github.com>
2022-01-26 12:11:29 +0300

    added an async method to the repository

--- Weather.СonsoleApp/Weather.DataAccess/Repositories/WeatherRepository.cs
+++ Weather.СonsoleApp/Weather.DataAccess/Repositories/WeatherRepository.cs
@@ -1,31 +1,18 @@
-﻿using System.Net;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Weather.DataAccess.Repositories.Abstrdact;
 using Newtonsoft.Json;
-using System.IO;
 using Weather.DataAccess.Models;
+using System.Net.Http;
 
 namespace Weather.DataAccess.Repositories
 {
     public class WeatherRepository : IWeatherRepositoty
     {
+        static readonly HttpClient _client = new HttpClient();
         public async Task<WeatherResponse> GetWeatherAsync(string cityName, string key)
         {
-            var responseWeather = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&appid={1}", cityName, key);
-              
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(responseWeather);
-
-            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-            string response;
-
-            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
-            {
-                response = streamReader.ReadToEnd();
-            }
-
-            var weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response);
-
+            var responce = await _client.GetStringAsync($"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid={key}");
+            var weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(responce);
             return weatherResponse;
         }
     }
