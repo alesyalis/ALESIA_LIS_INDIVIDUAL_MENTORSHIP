bbde9446f54e57d96a0ba7cb2a5558ba05633b7c
alesyalis <66536759+alesyalis@users.noreply.github.com>
2022-02-07 13:01:27 +0300

    remove empty  using

--- Weather.СonsoleApp/Weather.IntegrationTest/IConfigTest.cs
+++ Weather.СonsoleApp/Weather.IntegrationTest/IConfigTest.cs
@@ -1,17 +1,8 @@
 ﻿using AppConfiguration.AppConfig;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Weather.IntegrationTest
 {
     public class IConfigTest : IConfig
     {
-        //private string Url { get { return "https://api.openweathermap.org/data/2.5/weather?q"; } }
-
-        //string ApiKey { get { return "8e943ed8b016561c73b8a1920366ef79"; } }
         public string Url =>  "https://api.openweathermap.org/data/2.5/weather?q";
 
         public string ApiKey => "8e943ed8b016561c73b8a1920366ef79";
