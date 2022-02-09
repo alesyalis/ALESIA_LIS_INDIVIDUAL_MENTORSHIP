cc7b33d7de91d5959aa220aca8fcdec3b976d7f6
alesyalis <66536759+alesyalis@users.noreply.github.com>
2022-02-09 14:01:21 +0300

    fixed cpnfigTest

--- Weather.СonsoleApp/Weather.IntegrationTest/ConfigTest.cs
+++ Weather.СonsoleApp/Weather.IntegrationTest/ConfigTest.cs
@@ -5,18 +5,22 @@ namespace Weather.IntegrationTest
 {
     public class ConfigTest : IConfig
     {
-        public string Url { get; set; }
-
-        public string ApiKey { get; set; }
-
-        public ConfigTest()
+        public string Url
         {
-            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json")
-                                                   .Build();
-            ApiKey = config["ApiKey"];
-            Url = config["Url"];
+            get
+            {
+                return new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                                                 .Build()["url"];
+            }
+        }
+
+        public string ApiKey
+        {
+            get
+            {
+                return new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                                                 .Build()["ApiKey"];
+            }
         }
     }
-   
-
 }
