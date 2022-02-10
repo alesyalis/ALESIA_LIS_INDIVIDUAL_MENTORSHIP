fc00e91f3b8089384200be5d09d3238841109ade
alesyalis <66536759+alesyalis@users.noreply.github.com>
2022-02-10 13:40:08 +0300

    added 2 test for validator

--- Weather.СonsoleApp/Weather.Tests/Service/ValidatorTest.cs
+++ Weather.СonsoleApp/Weather.Tests/Service/ValidatorTest.cs
@@ -41,5 +41,46 @@ namespace Weather.Tests.Service
             // Assert
             Assert.DoesNotThrow(result);
         }
+
+        [TestCase("Minsk", 2)]
+        [TestCase("string", 5)]
+        public void ValidateForecast_CityNameReceived_Success(string name, int days)
+        {
+            // Arrange
+            // Act
+            void result() => _validator.ValidateForecast(name, days);
+
+            // Assert
+            Assert.DoesNotThrow(result);
+        }
+
+        [TestCase("", 6)]
+        [TestCase(null, 0)]
+        public void ValidateForecast_CityNameIsEmpty_ValidationMessage(string name, int days)
+        {
+            // Arrange
+            var message = "Entering the city is required\n";
+
+            // Act
+            void result() => _validator.ValidateForecast(name, days);
+
+            // Assert
+            Exception ex = Assert.Throws<ValidationException>(result);
+            Assert.AreEqual(message, ex.Message);
+        }
+
+        [TestCase("Minsk", 0)]
+        public void ValidateForecast_DaysIsEmpty_ValidationMessage(string name, int days)
+        {
+            // Arrange
+            var message = "Input number of days is required\n";
+
+            // Act
+            void result() => _validator.ValidateForecast(name, days);
+
+            // Assert
+            Exception ex = Assert.Throws<ValidationException>(result);
+            Assert.AreEqual(message, ex.Message);
+        }
     }
 }
