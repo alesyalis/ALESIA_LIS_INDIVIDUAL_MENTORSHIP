cde0775d2fdbb38790d3e1dac3f980e02c6797c5
alesyalis <66536759+alesyalis@users.noreply.github.com>
2022-02-10 12:08:12 +0300

    remove unnesrsary using

--- Weather.СonsoleApp/Weather.DataAccess/Models/LocationCity.cs
+++ Weather.СonsoleApp/Weather.DataAccess/Models/LocationCity.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Weather.DataAccess.Models
+﻿namespace Weather.DataAccess.Models
 {
     public class LocationCity
     {
